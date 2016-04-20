9307096022fea2c6ff9eba8ad4a2c84d7c5d2e76
salim <salim@AMZ>
2016-04-20 14:10:14 +0200

    testtoshiba1

--- src/main/java/com/mycompany/mavenproject3/App.java
+++ src/main/java/com/mycompany/mavenproject3/App.java
@@ -9,6 +9,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!!!!!!!" );
+        App app = new App
+               
+    }
+    /**
+     *  affiche persoone.
+     * 
+     * @param titi nom personne
+     */
+    public void toto(String titi)
+    {
+        
     }
 }
 
